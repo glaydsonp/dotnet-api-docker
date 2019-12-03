dbfa8f086027359a6d03f04c0440268ed1b5b44c
Glaydson Prado <glaydsonp@hotmail.com>
2019-12-03 10:33:06 -0300

    added storedatacontext, migrations, ef tools

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+// using ProductCatalog.Data.Maps;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost,8000;Database=prodcat;User ID=SA;Password=Root@123");
+        }
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.ApplyConfiguration(new ProductMap());
+        //     modelBuilder.ApplyConfiguration(new CategoryMap());
+        // }
+    }
+}
